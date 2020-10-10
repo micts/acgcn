e137f324b8c5a896f6606accd349203f520e5994
micts <tsiaousis.michail@gmail.com>
2020-10-10 21:21:24 +0200

    Add .sh run scripts for training and inference

---
+++ tools/test_baseline.sh
@@ -0,0 +1,1 @@
+python inference.py --model_name gcn --filename 2020-03-25_23-56-26 --model_instance epoch_300_1.108.pth --split test  
---
+++ tools/train_gcn.sh
@@ -0,0 +1,1 @@
+python main.py --model_name gcn --num_layers 2 --num_graphs 2 --merge_function concat
