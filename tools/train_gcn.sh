aa713a3e78613d15f543c99fa847a897208c6bf9
micts <tsiaousis.michail@gmail.com>
2020-10-17 15:21:07 +0200

    Add new argparse arguments

--- tools/train_gcn.sh
+++ tools/train_gcn.sh
@@ -1,1 +1,1 @@
-python tools/train.py --model_name gcn --num_layers 2 --num_graphs 2 --merge_function concat --total_epochs 450 --max_lr 4.7e-5 --batch_size 3
+python train.py --model_name gcn --num_layers 2 --num_graphs 2 --merge_function concat --total_epochs 450 --max_lr 4.7e-5 --batch_size 3
