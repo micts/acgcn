98d7adac2c763629d2f340da55cbcf92c995a6e3
micts <tsiaousis.michail@gmail.com>
2020-11-07 11:34:40 +0100

    Creates dataset (train, val, test) using a function

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,28 +1,26 @@
 from dataset import daly
 
-def return_dataset(cfg):
+def return_dataset(data_path, annot_path, cfg, split):
     
-    #annot_data = daly.gen_labels_keyframes(label_tracks=cfg.label_tracks, load_path=cfg.annot_path)
-    annot_data = daly.load_tracks(load_path=cfg.annot_path)
+    print('Preparing {} set...'.format(split))
+    annot_data = daly.load_tracks(load_path=annot_path)
+    frames = daly.get_frames(annot_path, annot_data, cfg, split=split, on_keyframes=False) 
+    dataset = daly.DALYDataset(data_path,
+                               annot_data, 
+                               frames,
+                               cfg,
+                               split=split)
+    print('Finished.\n')    
 
-    print('\nCreating training set...')
-    training_frames = daly.get_frames(annot_data, cfg, split='training') 
-    training_set = daly.DALYDataset(annot_data, 
-                                     training_frames,
-                                     cfg,
-                                     split='training')
+
+    #print('Creating validation set...')
+    #validation_frames = daly.get_frames(cfg.annot_path, annot_data, cfg, split='validation')
+    #validation_set = daly.DALYDataset(cfg.data_path,
+#				      annot_data, 
+#                                      validation_frames,
+#                                      cfg,
+#                                      split='validation')
     
-    print('Creating validation set...')
-    validation_frames = daly.get_frames(annot_data, cfg, split='validation')
-    validation_set = daly.DALYDataset(annot_data, 
-                                       validation_frames,
-                                       cfg,
-                                       split='validation')
-    
-    print('Finished.\n')
-    #print('%d training clips' % training_set.__len__())
-    #print('%d validation clips' % validation_set.__len__())
-    #print()
-    
-    return training_set, validation_set
+    #print('Finished.') 
+    return dataset
 
